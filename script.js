09e1505fbdca7137957f7fe2393e78eb92d0367d
brahimsouidene1994 <72886850+brahimsouidene1994@users.noreply.github.com>
2021-07-11 20:44:26 +0100

    animation

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+/**intro animation */
+const tl = gsap.timeline({defaults:{ease:"power1.out"}});
+
+tl.to('.text-hide',{y:'0%', duration:1, stagger:0.25});
+tl.to('.slider-intro',{y:'-100%', duration:1.5, delay:0.5});
+tl.to('.intro',{y:'-100%', duration:1},"-=1.25");
+tl.fromTo('.starting-text',{opacity:0},{opacity:1, duration:1});
+
+/**intro animation */
 /* hamburgor*/
 const hamburgor = document.querySelector(".hamburgor");
 const separator = document.querySelector(".separator-left");
@@ -16,3 +25,16 @@ mode.addEventListener('click',()=>{
     theme.classList.toggle('dark');
 })
 /** theme color */
+
+/**active link item */
+// Add active class to the current button (highlight it)
+var header = document.getElementById("my-navlinks");
+var btns = header.getElementsByClassName("link-item");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("active-link");
+  current[0].className = current[0].className.replace(" active-link", "");
+  this.className += " active-link";
+  });
+}
+/**active link item */
