e52c155e7c4299ec7883722aac8132ff0d8e8341
brahimsouidene1994 <72886850+brahimsouidene1994@users.noreply.github.com>
2021-07-11 01:07:49 +0100

    colors-change

--- script.js
+++ script.js
@@ -8,3 +8,23 @@ hamburgor.addEventListener('click',()=>{
     separator.classList.toggle('active');
 })
 /* hamburgor*/
+/** theme color */
+const theme = document.querySelector(":root");
+const mode = document.querySelector(".theme-mode");
+
+mode.addEventListener('click',()=>{
+    var rs = getComputedStyle(theme);
+    if(rs.getPropertyValue('--clr-bg') !== '#ade8f4'){
+        theme.style.setProperty('--clr-bg', '#ade8f4');
+        theme.style.setProperty('--clr-text', '#333533');
+        theme.style.setProperty('--clr-back-txt', 'rgb(202, 240, 248)');
+        theme.style.setProperty('--clr-back-txt-shadow', 'rgb(202, 240, 248, 0.5)');
+    }else{
+        theme.style.setProperty('--clr-bg', '#10002B');
+        theme.style.setProperty('--clr-text', '#C77DFF');
+        theme.style.setProperty('--clr-back-txt', 'rgb(38, 2, 100)');
+        theme.style.setProperty('--clr-back-txt-shadow', 'rgb(38, 2, 100, 0.5)');
+    }
+
+});
+/** theme color */
